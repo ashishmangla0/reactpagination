a20a6ccbd428c78801f91ea204bcb7ba6220342a
ashish <ashishmangla1991@gmail.com>
2023-01-02 19:10:44 +0530

    add new code

--- src/ReactPagination.js
+++ src/ReactPagination.js
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 
 const ReactPagination = () => {
   const [productsList, setProductsList] = useState([]);
-  const [page,setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
-  const ApiUrl = `https://dummyjson.com/products?limit=88`;
+  const ApiUrl = `https://dummyjson.com/products?limit=87`;
 
   const fetchProducts = async () => {
     const res = await fetch(ApiUrl);
@@ -15,24 +15,63 @@ const ReactPagination = () => {
   useEffect(() => {
     fetchProducts();
   }, []);
+  const handlePrev = () => {
+    setPage(page - 1);
+  };
+  const handleNext = () => {
+    setPage(page + 1);
+  };
+  const handlePageClick = (index) => {
+    setPage(index);
+  };
+  return (
+    <>
+      {productsList.length > 0 && (
+        <div className="products">
+          {productsList.slice(page * 10 - 10, page * 10).map((product) => {
+            return (
+              <div className="products__single" key={product.title}>
+                <img
+                  src={product.thumbnail}
+                  alt={product.title}
+                  className="products__img"
+                />
+                <div>{product.title}</div>
+              </div>
+            );
+          })}
+        </div>
+      )}
 
+      <div className="pagination">
+        <button
+         disabled={page > 1  ? false:  true}
+          onClick={handlePrev} className={"pagination__button"}>
+          {" "}
+          prev
+        </button>
+        {[...Array(Math.ceil(productsList.length / 10))].map((_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => handlePageClick(index + 1)}
+            className={`pagination__button ${
+              page === index + 1 ? "pagination__button--selected" : ""
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
 
-  return <>
-  {productsList.length > 0 && <div className="products">
-    {productsList.slice(page * 10 - 10, page * 10).map(()=><div>asdf</div>)}
-
-
-  </div>}
-  {productsList.map((product) => {return <div></div>} )}
-    <div>
-
-
-    </div>
-<div className="pagination">
-    I am pagination
-</div>
-
-  </>;
+        <button
+          disabled={page < Math.ceil(productsList.length / 10) ? false : true}
+          onClick={handleNext}
+          className={"pagination__button"}
+        >
+          Next{" "}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default ReactPagination;
